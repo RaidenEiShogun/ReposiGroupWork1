7df9b0c3de3c17adabaf65b7de280f102def520e
RaidenEiShogun <mariaabusaidhtl@gmail.com>
2023-12-05 23:55:10 +0200

    коммит продукта и заказа, в мейн джава должна быть прописана механика взаимодействия прописанных классов

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,44 @@
 public class Product {
 
+    private String name;
+    private Category category;
+    private int howManyInCart; //сколько в корзине
+    private double price;
+
+    public Product(String name, Category category, double price) {
+        this.name = name;
+        this.category = category;
+        this.howManyInCart = 0;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getHowManyInCart() {
+        return howManyInCart;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    //setter не всем нужен, ток для кол-ва в корзине
+    public void setHowManyInCart(int howManyInCart){
+        this.howManyInCart = howManyInCart;}
+
+    //добавляем товар в корзину
+    public void addToCurrentCart(int howMany){
+        this.howManyInCart +=howMany;}
+
+    //Ищем инфо на складе. Там имя и сколько на складе
+    public void viewWarehouse(Warehouse warehouse){
+        int howManyInWarehouse = warehouse.getNumberOfProductInWarehouse(this);
+        System.out.println("Product: "+ name+"\n"+"In warehouse: "+howManyInWarehouse);
+    }
 }
--- src/Purchase.java
+++ src/Purchase.java
@@ -1,29 +1,67 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Purchase {
-        private List<Product> productsInCart;
-        public Purchase(List<Product> productsInCart) {
-            this.productsInCart = productsInCart;
+    private final List<Product> productsInCart;
+    private final Warehouse warehouse;
+
+        public Purchase(Warehouse warehouse) {
+            this.productsInCart = new ArrayList<>();
+            this.warehouse = warehouse;
         }
 
-        public List<Product> getProductsInCart() {
-            return productsInCart;
-        }
+    public List<Product> getProductsInCart() {
+        return productsInCart;
+    }
+    //доступ в корзину
+    public void addToCart(Product product, int howMany){
+            //товар на складе есть? если да, то все добавляем и забираем со склада
+        if (warehouse.getNumberOfProductInWarehouse(product)>=howMany)
 
-        public void setProductsInCart(List<Product> productsInCart) {
-            this.productsInCart = productsInCart;
+        {
+            productsInCart.add(product);
+            warehouse.removeProductsFromWarehouse(product,howMany);
         }
+        else
+        {//иначе кидаю ошибку (хоть как-то связана с числом)
+            throw new IndexOutOfBoundsException("Not enough product: "+ product.getName() +"In a warehouse");
 
-        public void addToCart(Product product) {
-            this.productsInCart.add(product);
         }
+    }
 
-        public void processPurchase() {
-            // тут логика обработки процесса заказа
+    //подумаю как обработать заказ, а пока удалить заказ
+    public void dismissPurchase(){
+        for (Product p:
+             productsInCart) {
+            int howManyInCart = p.getHowManyInCart();
+            warehouse.addProductsToWarehouse(p,howManyInCart);
         }
+        productsInCart.clear(); // после всего чистим корзину
+    }
 
-        public void dismissPurchase() {
-            // логике отмены заказа
+    public void WorkOnPurchase(){
+        double howManyForAllPurchase = calcAll();
+
+        //нужно получить инфо для заказа
+        System.out.println("Processing purchase");
+        System.out.println("Products in your current cart");
+
+        for (Product p : productsInCart)
+        {
+            System.out.println("- " + p.getName() + " x" + p.getHowManyInCart()); //имя+сколько в корзине
         }
+        System.out.println("Total Price: " + howManyForAllPurchase+
+                "\nPurchase is offered. ");
 
+        productsInCart.clear(); //после всего чистим корзину
+    }
+
+    private double calcAll() {
+            double howManyForAllPurchase=0;
+        for (Product p:
+             productsInCart) {
+            howManyForAllPurchase+=p.getPrice()*p.getHowManyInCart();
+        }
+        return howManyForAllPurchase;
+    }
 }
